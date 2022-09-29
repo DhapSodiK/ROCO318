41b669a7911aa3bbae3857d3179b58b5c28b35a3
DhapSodiK <72078014+DhapSodiK@users.noreply.github.com>
2022-09-29 16:39:11 +0100

    Lab 1

---
+++ Lab 1/build/beginner_1/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/student/roco318_ws/devel/lib/beginner_1/talker"
+  "/home/student/roco318_ws/devel/lib/beginner_1/talker.pdb"
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab 1/build/beginner_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/roco318_ws/src/beginner_1
+# Build directory: /home/student/roco318_ws/build/beginner_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
