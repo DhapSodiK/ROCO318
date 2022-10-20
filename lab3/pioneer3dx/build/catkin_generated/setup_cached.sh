ab776f6771899f9bbee5165a3132b693c833d9a7
DhapSodiK <72078014+DhapSodiK@users.noreply.github.com>
2022-10-20 15:39:54 +0100

    Lab 3 Commit
    needs linking with copellia

---
+++ lab3/pioneer3dx/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/student/pioneer3dx/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/student/pioneer3dx/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/student/pioneer3dx/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/student/pioneer3dx/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/student/pioneer3dx/src:$ROS_PACKAGE_PATH"
