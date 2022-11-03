412d7482ce26145eed531813f0afcdb22d425305
DhapSodiK <72078014+DhapSodiK@users.noreply.github.com>
2022-11-03 16:12:05 +0000

    work

--- lab3/pioneer3dx/build/p3dx_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/lab3/pioneer3dx/src/p3dx_description
-# Build directory: /home/student/lab3/pioneer3dx/build/p3dx_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- lab3/pioneer3dx_simulation/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/lab3/pioneer3dx_simulation/src
-# Build directory: /home/student/lab3/pioneer3dx_simulation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("coppeliasim_p3dx_sim")
