abe5d9a43fa3f4e237909583b9c427d245db5c6f
DhapSodiK <72078014+DhapSodiK@users.noreply.github.com>
2022-11-24 17:00:51 +0000

    Juicy

---
+++ lab3/source_all.bash
@@ -0,0 +1,8 @@
+#! /bin/bash 
+
+PIONEER3DX_DRIVERS_WS=~/lab3/pioneer3dx
+PIONEER3DX_SIMULATION_WS=~/lab3/pioneer3dx_simulation
+
+source $PIONEER3DX_DRIVERS_WS/devel/setup.bash --extend
+source $PIONEER3DX_SIMULATION_WS/devel/setup.bash --extend
+
