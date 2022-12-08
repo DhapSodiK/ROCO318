5f493b3607371434399463c9a677be6a25648540
DhapSodiK <72078014+DhapSodiK@users.noreply.github.com>
2022-12-08 16:14:24 +0000

    Weird error

---
+++ lab3/float32/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/lab3/float32/src
+# Build directory: /home/student/lab3/float32/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("float32multiarray_to_pointcloud2")
---
+++ lab3/float32/build/float32multiarray_to_pointcloud2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/lab3/float32/src/float32multiarray_to_pointcloud2
+# Build directory: /home/student/lab3/float32/build/float32multiarray_to_pointcloud2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
